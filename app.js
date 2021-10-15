32ca87f1c48782f427663c445ab187ba7928adb7
taauntik <devauntik@gmail.com>
2021-10-15 21:11:29 +0600

    Task Manager project full stack

---
+++ app.js
@@ -0,0 +1,28 @@
+require("dotenv").config();
+const express = require("express");
+const app = express();
+const tasks = require("./routes/tasks");
+const connectDB = require("./db/connect");
+
+// middleware
+app.use(express.json());
+
+// routes
+app.get("/hello", (req, res) => {
+  res.send("Task Manager App");
+});
+
+app.use("/api/vi/tasks", tasks);
+
+const port = 3000;
+
+const start = async () => {
+  try {
+    await connectDB(process.env.MONGO_URI);
+    app.listen(port, console.log(`server is listening on port ${port}`));
+  } catch (err) {
+    console.log(err);
+  }
+};
+
+start();
